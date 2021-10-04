fa8ae2a8a0ab56065adfb863e48b10a3cf34d693
Okhotnik <vadim2000ohota@gmail.com>
2021-10-04 16:07:42 +0300

    .

--- src/main/java/com/model/Number.java
+++ src/main/java/com/model/Number.java
@@ -6,20 +6,15 @@ import java.util.Scanner;
 public class Number implements Numerical {
 
     @Override
-    public double explore (Scanner scanner){
-        String number;
-        number = scanner.nextLine();
+    public double explore(Scanner scanner) {
+        scanner.reset();
         double num;
         try {
-            Double.parseDouble(number);
-            num = Double.parseDouble(number);
+            num = Double.parseDouble(scanner.nextLine());
             return num;
         } catch (NumberFormatException e) {
-            System.out.println("Error. Enter 2 times any number!");
-            explore(scanner);
-            number = scanner.nextLine();
-            num = Double.parseDouble(number);
+            System.out.println("Error. Enter number!");
+            return explore(scanner);
         }
-        return num;
     }
 }
